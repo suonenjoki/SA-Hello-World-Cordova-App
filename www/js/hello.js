8c16f1bc65f4ac54440a556feb70cd7806f2988d
tdahlen@redhat.com <tdahlen@redhat.com>
2017-06-07 11:18:53 +0000

    update file /www/js/hello.js

--- www/js/hello.js
+++ www/js/hello.js
@@ -17,21 +17,3 @@ document.getElementById('say_hello').onclick = function () {
 };
 
 
-var myTableDiv = document.getElementById("cloudResponse");
-        var table = document.createElement('TABLE');
-        var tableBody = document.createElement('TBODY');
-
-        table.border = '1';
-
-        for (i = 0; i < res.length; i++) {
-          var tr = document.createElement('TR');
-          var td = document.createElement('TD');
-          td.appendChild(document.createTextNode(res[i]));
-          tr.appendChild(td);
-          tableBody.appendChild(tr);
-        }
-
-        table.appendChild(tableBody);
-
-        myTableDiv.innerHTML = " ";
-        myTableDiv.appendChild(table);
