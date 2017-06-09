f557abfeed328f7d9347e066e7466306a4f1c3ce
tdahlen@redhat.com <tdahlen@redhat.com>
2017-06-09 09:10:22 +0000

    update file /www/js/form.js

--- www/js/form.js
+++ www/js/form.js
@@ -13,7 +13,26 @@ $fh.forms.init({config: {}}, function(err){
         } else {
             var formsList = forms.getFormsList();
 
-            
+
+            var myTableDiv = document.getElementById("sync-div");
+            var table = document.createElement('TABLE');
+            var tableBody = document.createElement('TBODY');
+
+            table.border = '1';
+
+            for (var i = 0; i < products.length; i++) {
+                var tr = document.createElement('TR');
+                var td = document.createElement('TD');
+                td.appendChild(document.createTextNode(products[i].productname));
+                tr.appendChild(td);
+                tableBody.appendChild(tr);
+            }
+
+            table.appendChild(tableBody);
+
+            myTableDiv.innerHTML = " ";
+            myTableDiv.appendChild(table);
+
 
 
         }
