c35a3616d6735b48a706aa886b959d1789d6ecc1
tdahlen@redhat.com <tdahlen@redhat.com>
2017-06-09 10:38:21 +0000

    update file /www/js/form.js

--- www/js/form.js
+++ www/js/form.js
@@ -9,26 +9,7 @@ $fh.forms.getForms({"fromRemote": false}, function(err, forms){
     if(err) {
         console.log("Error getting Forms " + JSON.stringify(err));
     } else {
-        var formsList = forms.getFormsList();
-        console.log("Forms: " + JSON.stringify(formsList));
-
-        var myTableDiv = document.getElementById("form-div");
-        var table = document.createElement('TABLE');
-        var tableBody = document.createElement('TBODY');
-
-        table.border = '1';
-
-        for (var i = 0; i < formsList.length; i++) {
-            var tr = document.createElement('TR');
-            var td = document.createElement('TD');
-            td.appendChild(document.createTextNode(formsList[i]));
-            tr.appendChild(td);
-            tableBody.appendChild(tr);
-        }
-
-        table.appendChild(tableBody);
-        myTableDiv.innerHTML = " ";
-        myTableDiv.appendChild(table);
+        console.log("Forms: " + JSON.stringify(forms));
     }
 });
 
