f2d53eca88226e1086d8c7590271d83d91349a49
tdahlen@redhat.com <tdahlen@redhat.com>
2017-06-09 09:09:40 +0000

    update file /www/js/form.js

--- www/js/form.js
+++ www/js/form.js
@@ -1,5 +1,25 @@
 $fh.forms.init({config: {}}, function(err){
     if(err){
         alert("Error Initialising Forms " + err);
+    } else {
+
+    var params = {
+    "fromRemote": true
+    };
+
+    $fh.forms.getForms(params, function(err, forms){
+        if(err) {
+            alert(err);
+        } else {
+            var formsList = forms.getFormsList();
+
+            
+
+
+        }
+    });
+
+
+
     }
-}
+});
