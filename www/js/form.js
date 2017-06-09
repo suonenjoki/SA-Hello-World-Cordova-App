31846624c77cef1ab896d96232014baf64eccc3f
tdahlen@redhat.com <tdahlen@redhat.com>
2017-06-09 10:04:45 +0000

    update file /www/js/form.js

--- www/js/form.js
+++ www/js/form.js
@@ -2,37 +2,33 @@ $fh.forms.init({}, function(err){
     if(err){
         console.log("Error Initialising Forms " + err);
         alert("Error Initialising Forms " +JSON.stringify(err));
-    } else {
-
-        $fh.forms.getForms({"fromRemote": true}, function(err1, forms){
-            if(err1) {
-                console.log("Error getting Forms " + JSON.stringify(err1));
-                alert(err1);
-            } else {
-                var formsList = forms.getFormsList();
-                console.log("Forms: " + JSON.stringify(formsList));
-
-                var myTableDiv = document.getElementById("form-div");
-                var table = document.createElement('TABLE');
-                var tableBody = document.createElement('TBODY');
-
-                table.border = '1';
-
-                for (var i = 0; i < formsList.length; i++) {
-                    var tr = document.createElement('TR');
-                    var td = document.createElement('TD');
-                    td.appendChild(document.createTextNode(formsList[i]));
-                    tr.appendChild(td);
-                    tableBody.appendChild(tr);
-                }
-
-                table.appendChild(tableBody);
-
-                myTableDiv.innerHTML = " ";
-                myTableDiv.appendChild(table);
-
-            }
-        });
-
     }
 });
+
+$fh.forms.getForms({"fromRemote": true}, function(err, forms){
+    if(err) {
+        console.log("Error getting Forms " + JSON.stringify(err1));
+    } else {
+        var formsList = forms.getFormsList();
+        console.log("Forms: " + JSON.stringify(formsList));
+
+        var myTableDiv = document.getElementById("form-div");
+        var table = document.createElement('TABLE');
+        var tableBody = document.createElement('TBODY');
+
+        table.border = '1';
+
+        for (var i = 0; i < formsList.length; i++) {
+            var tr = document.createElement('TR');
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(formsList[i]));
+            tr.appendChild(td);
+            tableBody.appendChild(tr);
+        }
+
+        table.appendChild(tableBody);
+        myTableDiv.innerHTML = " ";
+        myTableDiv.appendChild(table);
+    }
+});
+
