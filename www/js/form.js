a7e9d72cf8791619585d9b3585e5977ce84156b6
tdahlen@redhat.com <tdahlen@redhat.com>
2017-06-09 09:16:46 +0000

    update file /www/js/form.js

--- www/js/form.js
+++ www/js/form.js
@@ -1,44 +1,38 @@
 $fh.forms.init({config: {}}, function(err){
     if(err){
+        console.log("Error Initialising Forms " + err);
         alert("Error Initialising Forms " + err);
     } else {
 
-    var params = {
-    "fromRemote": true
-    };
+        var params = {"fromRemote": true};
 
-    $fh.forms.getForms(params, function(err, forms){
-        if(err) {
-            alert(err);
-        } else {
-            var formsList = forms.getFormsList();
+        $fh.forms.getForms(params, function(err, forms){
+            if(err) {
+                alert(err);
+            } else {
+                var formsList = forms.getFormsList();
 
+                var myTableDiv = document.getElementById("form-div");
+                var table = document.createElement('TABLE');
+                var tableBody = document.createElement('TBODY');
 
-            var myTableDiv = document.getElementById("form-div");
-            var table = document.createElement('TABLE');
-            var tableBody = document.createElement('TBODY');
+                table.border = '1';
 
-            table.border = '1';
+                for (var i = 0; i < formsList.length; i++) {
+                    var tr = document.createElement('TR');
+                    var td = document.createElement('TD');
+                    td.appendChild(document.createTextNode(formsList[i]));
+                    tr.appendChild(td);
+                    tableBody.appendChild(tr);
+                }
 
-            for (var i = 0; i < formsList.length; i++) {
-                var tr = document.createElement('TR');
-                var td = document.createElement('TD');
-                td.appendChild(document.createTextNode(formsList[i]));
-                tr.appendChild(td);
-                tableBody.appendChild(tr);
+                table.appendChild(tableBody);
+
+                myTableDiv.innerHTML = " ";
+                myTableDiv.appendChild(table);
+
             }
-
-            table.appendChild(tableBody);
-
-            myTableDiv.innerHTML = " ";
-            myTableDiv.appendChild(table);
-
-
-
-        }
-    });
-
-
+        });
 
     }
 });
